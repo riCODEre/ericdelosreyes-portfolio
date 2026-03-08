2d963419618aad1aee46f5e505203e9ea3daf8c3
Ricodere <thericodere@gmail.com>
2026-03-09 01:14:04 +0800

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,97 @@
-import Image from "next/image";
+'use client';
+import { useState } from "react";
+import { Sun, Moon } from "lucide-react";
+import Image from 'next/image'
 
 export default function Home() {
+  // all UseState
+  const [themeMode, setThemeMode] = useState<'light' | 'dark'>('light')
+
+  // all Toggles
+  const toggleTheme = () => {
+    setThemeMode(prev => (prev == 'light' ? 'dark' : 'light'))
+  }
+
+  // others
+  const themeClasses = themeMode === 'light' ? 'bg-white text-black' : 'bg-black text-white'
+  const heroThemeClasses = themeMode === 'light' ? 'bg-linear-to-r from-white via-white/95 to-white/80 text-black' : 'bg-linear-to-r from-black via-black/95 to-black/80 text-white'
+  function ThemeButton (){
+    if (themeMode === 'light') {
+      return (
+        <Sun size={24}></Sun>
+      )
+    }
+    else{
+      return (
+        <Moon size={24}></Moon>
+      )
+    }
+    
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
+    <div className={`${themeClasses} min-h-screen`}>
+      <header className=" border-b border-gray-500/30 backdrop-blur-md py-5 px-10 fixed top-0 right-0 left-0 z-50">
+        <nav className="flex justify-between items-center ">
+          <a type="" href="#" className="uppercase font-mono text-cyan-500 text-lg font-bold " 
+          style={{ textShadow: "rgba(0, 234, 255, 0.8) 0px 0px 10px, rgba(0, 234, 255, 0.4) 0px 0px 30px, rgba(0, 234, 255, 0.2) 0px 0px 60px" }}>
+            &lt;Eric Delos Reyes&gt;
           </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+          <div className="flex justify-end items-center gap-10">
+            <NavLink link="#About" number="01" text="About"></NavLink>
+            <NavLink link="#About" number="02" text="Skills"></NavLink>
+            <NavLink link="#About" number="03" text="Projects"></NavLink>
+            <NavLink link="#About" number="03" text="Contact"></NavLink>
+            <button className="cursor-pointer border hover:shadow-[0_0_20px_5px_rgba(59,130,246,0.5)] border-cyan-500 p-2" onClick={toggleTheme}>
+              <ThemeButton/>
+            </button>
+          </div> 
+        </nav>
+      </header>
+      <main className="">
+        <section id="hero" className="relative min-h-screen">
+          <div className="absolute inset-0 scanline pointer-events-none z-30"></div>
+          <div className="absolute inset-0">
+            <img src="/images/heroimage.jpg" alt="" className="z-10 w-full h-full object-cover" />
+          </div>
+          <div className={`pt-20 absolute z-20 ${heroThemeClasses} w-full h-full flex justify-between`}>
+            <div className="w-1/2">
+                dawd
+            </div>
+            <div className="w-1/2 flex justify-start mt-10">
+              <div className="rounded-2xl w-fit h-fit text-cyan-500 shadow-[0_0_20px_5px_rgba(0,184,219,0.5)] bg-linear-to-br from-cyan-500/50 via-transparent to-transparent">
+                <div className="relative">
+                  <Image
+                    src={'/images/profilepic.png'}
+                    width={350}
+                    height={350}
+                    alt=""
+                  />
+                  <div className="absolute w-1/11 h-1/13 border-t border-l left-5 top-5 border-cyan-500"></div>
+                  <div className="absolute w-1/11 h-1/13 border-t border-r right-5 top-5 border-cyan-500"></div>
+                  <div className="absolute w-1/11 h-1/13 border-b border-l left-5 bottom-5 border-red-500"></div>
+                  <div className="absolute w-1/11 h-1/13 border-b border-r right-5 bottom-5 border-red-500"></div>
+
+                </div>
+              </div>
+            </div>
+            
+          </div>      
+        </section>
+        
       </main>
     </div>
-  );
+    
+  )
+}
+
+
+function NavLink({link, number, text}: {link:string, number:string, text:string}){
+  
+  return (
+    <a href={link} className="space-x-3 hover:text-cyan-500">
+      <span className="text-red-500">{number}.</span>
+      <span>{text}</span>
+    </a>
+  )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        myfont: ['var(--font-myfont)', 'sans-serif'],
+      },
+    },
+  },
+};
