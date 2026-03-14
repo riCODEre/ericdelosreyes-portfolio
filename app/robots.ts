45af103fbe707d7b2f8e56d0029e6ea1443791ed
Ricodere <thericodere@gmail.com>
2026-03-14 20:50:46 +0800

    removing a json unneeded

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: "/terminal",
     },
     sitemap: "https://ericdelosreyes.com/sitemap.xml",
   };
