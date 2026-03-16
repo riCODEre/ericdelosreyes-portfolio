6a003488150411e3eac43b24c7f8896c68d541af
Ricodere <thericodere@gmail.com>
2026-03-17 00:35:26 +0800

    fix the useragent

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/terminal",
+      disallow: "/fixer",
     },
     sitemap: "https://ericdelosreyes.com/sitemap.xml",
   };
